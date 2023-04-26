fbea1cebfa0d53765f4c927fd09e81cdf693999a
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2023-04-25 21:46:24 -0400

    Made Image Map for first Image

--- AvationPlaneTest/Game.aspx
+++ AvationPlaneTest/Game.aspx
@@ -7,12 +7,134 @@
     <style>
         /* Set the page background color */
         #image {
-            background-size: cover;
-            width: 100%;
-            
+            background-image: url(~/Background2.png);
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            height: 1080px;
         }
+        #imageMapBox {
+            background-image: url(~/ImageMapBox.png);
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            position: relative;
+            
 
-    </style>
+
+        #imageMapImage {
+            background-image: url(~/Control1.png);
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            top: 0px;
+            left: -560px;
+        }
+        #imageb {
+            margin: 0 auto;
+            padding: 100px;
+            height: 406px;
+        }
+.textPanel {
+  width: 50%;
+  height: auto;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  padding: 20px;
+}
+.textPanelQ {
+  width: 75%;
+  height: auto;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  padding: 20px;
+}
+/* Set the text label properties */
+.textLabel {
+  font-family: Arial;
+  font-size: 16px;
+  color: white;
+  word-wrap: break-word;
+}
+.divAbtn{
+    display: flex;
+    flex-direction: row;
+}
+}
+
+</head>
+<body>
+
+    <asp:Panel ID="image" runat="server" Height="1080" BackImageUrl="~/Background2.png" CssClass="image">
+
+        <div id="content">
+
+            <!--image id="imageMapBox" src="ImageMapBox.png">
+            </-->
+
+            <asp:ImageMap 
+                ID="imageMapImage" 
+                runat="server" 
+                ImageUrl="~/Control1.png"  
+                CssClass="imageMapImage" 
+                Height="700px" 
+                Width="550px" 
+                OnClick="saveButtonPress">
+
+                <asp:RectangleHotSpot          
+                    top="59"
+                    left="364"
+                    bottom="92"
+                    right="405"
+                    hotspotmode="PostBack"
+                    postbackvalue="0">
+                </asp:RectangleHotSpot> 
+
+                <asp:RectangleHotSpot          
+                    top="44"
+                    left="63"
+                    bottom="106"
+                    right="110"
+                    hotspotmode="PostBack"
+                    postbackvalue="1">
+                </asp:RectangleHotSpot> 
+
+                <asp:RectangleHotSpot          
+                    top="256"
+                    left="245"
+                    bottom="309"
+                    right="296"
+                    hotspotmode="PostBack"
+                    postbackvalue="2">
+                </asp:RectangleHotSpot> 
+
+                <asp:RectangleHotSpot          
+                    top="158"
+                    left="424"
+                    bottom="216"
+                    right="535"
+                    hotspotmode="PostBack"
+                    postbackvalue="3">
+                </asp:RectangleHotSpot> 
+
+                <asp:RectangleHotSpot          
+                    top="220"
+                    left="422"
+                    bottom="268"
+                    right="533"
+                    hotspotmode="PostBack"
+                    postbackvalue="4">
+                </asp:RectangleHotSpot> 
+
+            </asp:ImageMap>
+            </div> 
+
+        <div class="right">
+        <div class="divA">
+            <asp:Panel ID="pnlQ" runat="server" BackImageUrl="~/Images/PromptBox.png" CssClass="textPanelQ">
+                <asp:Label ID="lblQ" runat="server" CssClass="textLabel" Text=".askhajksdhkajhsdjkasdjkaksdhakjsdkajhsdklajhsdkahskahsdjkahksdjlhaksjldhajklsdh"></asp:Label>
+                
+            </asp:Panel>
+        </div>
+
+
 
         <asp:Panel ID="image" runat="server" Height ="1080" BackImageUrl="~/Background2.png" CssClass="image">
 
--- AvationPlaneTest/Game.aspx.cs
+++ AvationPlaneTest/Game.aspx.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +12,174 @@ namespace AvationPlaneTest
 {
     public partial class Game : System.Web.UI.Page
     {
+        private int intButtonSelected;
+        private int intTotalQuestions = 3;
+        private int intCurrentQuestion;
+        private int intCurrentQuestionCounter = 0;
+        private int intCorrectButton;
+        private int intnumCorrect =0;
+        List<Questions> questions;
+        List<Label> lblListA;
+        Random random = new Random();
         protected void Page_Load(object sender, EventArgs e)
         {
+            lblConsole.Text = "Console:";
+            if (ViewState["loaded"] != null)
+            {
+                return;
+            }
+            ViewState["loaded"] = "true";
+            ViewState["intCorrectButton"] = 0;
+            ViewState["intButtonSelected"] = 0;
+            ViewState["intPoints"] = 0;
+            ViewState["intCurrentQuestionCounter"] = 0;
+            ViewState["currentQuestion"] = 0;
+            ViewState["currentButton"] = -1;
+            setQuestions();
+        }
+
+        protected void imgbtnA1_Click(object sender, ImageClickEventArgs e)
+        {
+            if (ViewState["GameOver"] != null)
+                return;
+            deselectButtons();
+            imgbtnA1.ImageUrl = "~/Images/ChoiceButtonOn.png";
+            ViewState["intButtonSelected"] = 1;
+        }
+
+        protected void imgbtnA2_Click(object sender, ImageClickEventArgs e)
+        {
+            if (ViewState["GameOver"] != null)
+                return;
+            deselectButtons();
+            imgbtnA2.ImageUrl = "~/Images/ChoiceButtonOn.png";
+            ViewState["intButtonSelected"] = 2;
+        }
+
+        protected void imgbtnA3_Click(object sender, ImageClickEventArgs e)
+        {if (ViewState["GameOver"] != null)
+                return;
+            deselectButtons();
+            imgbtnA3.ImageUrl = "~/Images/ChoiceButtonOn.png";
+            ViewState["intButtonSelected"] = 3;
+        }
+
+        protected void imgbtnA4_Click(object sender, ImageClickEventArgs e)
+        {if (ViewState["GameOver"] != null)
+                return;
+            deselectButtons();
+            imgbtnA4.ImageUrl = "~/Images/ChoiceButtonOn.png";
+            ViewState["intButtonSelected"] = 4;
+        }
+
+        // changes the buttons image to disable and sets the selection var to 0
+        private void deselectButtons()
+        {
+            imgbtnA1.ImageUrl = "~/Images/ChoiceButtonOff.png";
+            imgbtnA2.ImageUrl = "~/Images/ChoiceButtonOff.png";
+            imgbtnA3.ImageUrl = "~/Images/ChoiceButtonOff.png";
+            imgbtnA4.ImageUrl = "~/Images/ChoiceButtonOff.png";
+            ViewState["intButtonSelected"] = 0;
+        }
+
+        protected void imgbtnSubmit_Click(object sender, ImageClickEventArgs e)
+        {
+            if (ViewState["GameOver"] != null)
+                return;
+            //lblQ.Text = ViewState["intButtonSelected"].ToString() + ViewState["intCorrectButton"];
+            if (ViewState["intButtonSelected"].ToString() == ViewState["intCorrectButton"].ToString()
+                && ViewState["currentButton"] == ViewState["currentQuestion"])
+            {
+                lblQ.Text = "you did the thing";
+                ViewState["intPoints"] = (int)ViewState["intPoints"] + 1;
+            }
+            deselectButtons();
+            if((int)ViewState["intCurrentQuestionCounter"] >= intTotalQuestions)
+            {
+                ViewState["GameOver"] = "true";
+
+                // end game and show results here
+
+                lblConsole.Text = lblConsole.Text + " game over, " + ViewState["intPoints"].ToString() + " points";
+            }
+            else
+            {
+                //next question
+                setQuestions();
+            }
+        }
+        //checks if the selected hotspot on the image map is the correct answer
+        protected void saveButtonPress(Object sender, ImageMapEventArgs e)
+        {
+            ViewState["currentButton"] = e.PostBackValue;  
+            lblConsole.Text = "works";
+        }
+
+        private void setQuestions()
+        {
+            lblListA = new List<Label>
+            {
+                lblA1,
+                lblA2,
+                lblA3,
+                lblA4
+            };
+
+            // reads the question file
+            string json = File.ReadAllText(Server.MapPath(@"~/questions.json"));
+
+            // sets the options when converting the json
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+
+            // string to object
+            questions = System.Text.Json.JsonSerializer.Deserialize<List<Questions>>(json, options);
+
+            intCurrentQuestion = random.Next(6);
+            ViewState["currentQuestion"] = intCurrentQuestion;
+
+            lblQ.Text = questions[intCurrentQuestion].strQuestion;
+
+            // hash holds random answers
+            HashSet<int> numbers = new HashSet<int>
+            {
+                intCurrentQuestion
+            };
+            // get random answers
+            while (numbers.Count < 4)
+            {
+                int num = random.Next(6);
+
+                // add to hash
+                numbers.Add(num);
+            }
+            var hashValues = numbers.ToArray();
+            var rndButtons = Enumerable.Range(0, 4).OrderBy(n => random.Next());
+
+            // Take 4
+            var result = rndButtons.Take(4);
+
+            int i = 0;
+            foreach (int num in result)
+            {
+                // what button is the correct answer
+                if (intCurrentQuestion == hashValues[i])
+                    ViewState["intCorrectButton"] = num + 1;
+                lblListA[num].Text = questions[hashValues[i++]].strAnswer;
+            }
+            ViewState["intCurrentQuestionCounter"] = (int)ViewState["intCurrentQuestionCounter"] + 1;
 
         }
+    }
+
+    // structure to store questions and answers
+    public class Questions
+    {
+        public string strQuestion { get; set; }
+        public string strAnswer { get; set; }
+        public int intControl { get; set; }
     }
 }
